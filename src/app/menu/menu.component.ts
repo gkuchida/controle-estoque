b4a25a8d01dce8dfdfdac5210d0d029f367e42f6
Glaucia <gkuchida@hotmail.com>
2023-06-22 15:30:56 -0300

    gitflow-feature-stash: atividade-13

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Injectable, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-menu',
@@ -7,4 +9,27 @@ import { Component } from '@angular/core';
 })
 export class MenuComponent {
 
+  constructor(private router: Router, private location: Location){}
+
+  voltar(){
+    this.location.back();
+  }
+  isCadastro(){
+    return this.router.url.includes('/cadastro');
+  }
+  isVisualizar(){
+    return this.router.url.includes('/visualizar');
+  }
+  isDetalhes(){
+    return this.router.url.includes('/detalhes');
+  }
+  isExcluir(){
+    return this.router.url.includes('/excluir');
+  }
+  isAtualizar(){
+    return this.router.url.includes('/atualizar');
+  }
+  isEditar(){
+    return this.router.url.includes('/editar');
+  }
 }
